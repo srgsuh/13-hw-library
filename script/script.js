be72f50902850e1713cf00794beb59ec5b0ea847
srgsuh <srg.suhoverhov@gmail.com>
2025-05-21 22:54:08 +0300

    Add basic styling and update input form elements

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
 const library = new Library();
-while (true) {
-    //isbn;title;author;year
-    const inputStr = prompt('Enter book data (format ISBN;Title;Author;YYYY)):');
-    if (!inputStr){
-        break;
-    }
-    library.addUniqueBook(Book.of(inputStr));
-}
+// while (true) {
+//     //isbn;title;author;year
+//     const inputStr = prompt('Enter book data (format ISBN;Title;Author;YYYY)):');
+//     if (!inputStr){
+//         break;
+//     }
+//     library.addUniqueBook(Book.of(inputStr));
+// }
 // library.addUniqueBook(new Book('0001', 'Cat\'s Cradle', 'Kurt Vonnegut', 1963));
 // library.addUniqueBook(new Book('1984', 'Nineteen Eighty-Four', 'George Orwell', 1949));
 // library.addUniqueBook(new Book('0001', 'We', 'Eugene Zamiatin', 1924));
 // library.addUniqueBook(new Book('0002', 'Brave New World', 'Aldous Huxley', 1932));
 // library.print();
-
-const ol = document.createElement('ol');
-library.forEach((book) => {
-    const li = document.createElement('li');
-    const text = document.createTextNode(book);
-    li.appendChild(text);
-    ol.appendChild(li);
-});
-document.body.appendChild(ol);
+//
+// const ol = document.createElement('ol');
+// library.forEach((book) => {
+//     const li = document.createElement('li');
+//     const text = document.createTextNode(book);
+//     li.appendChild(text);
+//     ol.appendChild(li);
+// });
+// document.body.appendChild(ol);
