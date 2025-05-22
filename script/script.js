ec6af84f4beb6cc6d3dcb686c8beff4050f8002f
srgsuh <srg.suhoverhov@gmail.com>
2025-05-22 14:29:40 +0300

    Comment out outdated test data in script.js

--- script/script.js
+++ script/script.js
@@ -9,21 +9,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     library.init();
 
-    form.isbn.value = '8890-1234';
-    form.title.value = 'The Hobbit';
-    form.author.value = 'John Ronald Reuel Tolkien';
-    form.year.value = 1937;
-    form.addBookBtn.click();
-
-    form.isbn.value = 'AAAA-0001';
-    form.title.value = 'The Invisible Man';
-    form.author.value = 'Herbert George Wells';
-    form.year.value = 1897;
-    form.addBookBtn.click();
-
-    form.isbn.value = 'AAAA-0001';
-    form.title.value = '';
-    form.author.value = 'Herbert George Wells';
-    form.year.value = 1237;
-    form.addBookBtn.click();
+    // form.isbn.value = '8890-1234';
+    // form.title.value = 'The Hobbit';
+    // form.author.value = 'John Ronald Reuel Tolkien';
+    // form.year.value = 1937;
+    // form.addBookBtn.click();
+    //
+    // form.isbn.value = 'AAAA-0001';
+    // form.title.value = 'The Invisible Man';
+    // form.author.value = 'Herbert George Wells';
+    // form.year.value = 1897;
+    // form.addBookBtn.click();
+    //
+    // form.isbn.value = 'AAAA-0001';
+    // form.title.value = 'Diamond Sutra';
+    // form.author.value = '';
+    // form.year.value = 868;
+    // form.addBookBtn.click();
 });
